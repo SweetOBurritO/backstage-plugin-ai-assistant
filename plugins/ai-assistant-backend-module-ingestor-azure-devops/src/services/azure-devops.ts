69b78b02cf11d7239c02aa0310563177a11350f4
Abdullah Hassen <50408869+amhassen97@users.noreply.github.com>
2025-09-30 11:22:39 +0200

    feat(wip): add ado service

---
+++ plugins/ai-assistant-backend-module-ingestor-azure-devops/src/services/azure-devops.ts
@@ -0,0 +1,92 @@
+import {
+  LoggerService,
+  RootConfigService,
+} from '@backstage/backend-plugin-api';
+import { getPersonalAccessTokenHandler, WebApi } from 'azure-devops-node-api';
+import { VersionControlRecursionType } from 'azure-devops-node-api/interfaces/GitInterfaces';
+
+export const createAzureDevOpsService = ({
+  config,
+  logger,
+}: {
+  config: RootConfigService;
+  logger: LoggerService;
+}) => {
+  const organization = config.getString(
+    'aiAssistant.ingestors.azureDevOps.organization',
+  );
+  const project = config.getString('aiAssistant.ingestors.azureDevOps.project');
+  const token = config.getString('aiAssistant.ingestors.azureDevOps.token');
+
+  const orgUrl = `https://dev.azure.com/${organization}`;
+
+  logger.info(
+    `Connecting to Azure DevOps organization: ${organization}, project: ${project}`,
+  );
+
+  if (!organization || !project || !token) {
+    throw new Error(
+      'Azure DevOps organization, project, and token are required',
+    );
+  }
+
+  const authHandler = getPersonalAccessTokenHandler(token);
+
+  const connection = new WebApi(orgUrl, authHandler);
+
+  const getRepos = async () => {
+    const gitApi = await connection.getGitApi();
+
+    const repos = await gitApi.getRepositories(project);
+
+    logger.info(`Found ${repos.length} repositories in project ${project}`);
+
+    return repos;
+  };
+
+  const getRepoItems = async (repoId: string, fileTypes?: string[]) => {
+    const gitApi = await connection.getGitApi();
+
+    const items = await gitApi.getItems(
+      repoId,
+      project,
+      undefined,
+      VersionControlRecursionType.Full,
+    );
+
+    logger.info(
+      `Found ${items.length} items in Azure DevOps repository ${repoId}`,
+    );
+
+    if (fileTypes && fileTypes.length > 0) {
+      const filteredItems = items.filter(
+        item =>
+          !item.isFolder && fileTypes.some(type => item.path?.endsWith(type)),
+      );
+      logger.info(
+        `Filtered to ${filteredItems.length} items with types: ${fileTypes.join(
+          ', ',
+        )}`,
+      );
+      return filteredItems;
+    }
+
+    return items;
+  };
+
+  const getRepoItemContent = async (repoId: string, path: string) => {
+    const gitApi = await connection.getGitApi();
+
+    const itemContent = await gitApi.getItemContent(
+      repoId,
+      path,
+      project,
+      undefined,
+      VersionControlRecursionType.None,
+    );
+
+    return itemContent;
+  };
+
+  return { getRepos, getRepoItems, getRepoItemContent };
+};
